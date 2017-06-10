be02a4c4224229bb2444b97f147ae2026fe05d80
Peter Leung <babyghost@gmail.com>
2017-06-10 23:19:48 +0100

    Initial Commit

---
+++ LiveCoreML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiveCoreML
+//
+//  Created by Peter Leung on 10/6/2017.
+//  Copyright © 2017 winandmac Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
